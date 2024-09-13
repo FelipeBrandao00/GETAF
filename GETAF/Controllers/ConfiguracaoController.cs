d0c4f0198b3aea13ef20e57276e89e5ebdb56c6e
Framboezu <noxusghost@gmail.com>
2024-09-13 16:40:05 -0300

    Estrutura da funçao de Configuraçoes

---
+++ GETAF/Controllers/ConfiguracaoController.cs
@@ -0,0 +1,39 @@
+﻿using GETAF.Models;
+using GETAF.Models.Context;
+using GETAF.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GETAF.Controllers
+{
+    public class ConfiguracaoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult EditarNome([FromBody] ConfiguracaoModel config)
+        {
+            var retorno = config.EditarNome();
+            return Json(retorno);
+        }
+
+        public JsonResult EditarEmail([FromBody] ConfiguracaoModel config)
+        {
+            var retorno = config.EditarEmail();
+            return Json(retorno);
+        }
+
+        public JsonResult EditarSenha([FromBody] ConfiguracaoModel config)
+        {
+            var retorno = config.EditarPassword();
+            return Json(retorno);
+        }
+
+        public JsonResult EditarFoto([FromBody] ConfiguracaoModel config)
+        {
+            var retorno = config.EditarFoto();
+            return Json(retorno);
+        }
+    }
+}
---
+++ GETAF/Models/ViewModel/ConfiguracaoModel.cs
@@ -0,0 +1,34 @@
+﻿using GETAF.Models.Context;
+
+namespace GETAF.Models.ViewModel
+{
+    public class ConfiguracaoModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public byte[]? Foto { get; set; }
+
+
+        public Resposta EditarNome()
+        {
+            return null;
+        }
+
+        public Resposta EditarEmail()
+        {
+            return null;
+        }
+
+        public Resposta EditarPassword()
+        {
+            return null;
+        }
+
+        public Resposta EditarFoto()
+        {
+            return null;
+        }
+
+    }
+}
