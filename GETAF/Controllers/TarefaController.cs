8b400e0e6728c0681f84f279a300ecb7562a43e9
Framboezu <noxusghost@gmail.com>
2024-11-01 14:22:59 -0300

    config pt1

---
+++ GETAF/Controllers/TarefaController.cs
@@ -0,0 +1,18 @@
+﻿using GETAF.Models.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GETAF.Controllers
+{
+    public class TarefaController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public TarefaController(AppDbContext context) { _context = context; }
+
+        public IActionResult Index()
+        {
+            var tarefas = _context.Tarefas.ToList();
+            return View(tarefas);
+        }
+    }
+}
