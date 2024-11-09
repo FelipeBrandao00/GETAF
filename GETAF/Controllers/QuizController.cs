a1171eadb2c2208f9e30f47dda91d027fed8327a
Framboezu <noxusghost@gmail.com>
2024-11-09 12:33:33 -0300

    Criando QuizController e Model + Métodos

---
+++ GETAF/Controllers/QuizController.cs
@@ -0,0 +1,38 @@
+﻿using GETAF.Models.Context;
+using GETAF.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GETAF.Controllers
+{
+    public class QuizController : Controller
+    {
+        private readonly AppDbContext _context;
+        public QuizController(AppDbContext context) { _context = context; }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CriarQuiz([FromBody] QuizModel quizModel)
+        {
+            var quiz = quizModel.CriarQuiz(_context);
+            return null;
+        }
+
+        public IActionResult ListarQuiz([FromBody] QuizModel quizModel)
+        {
+            return null;
+        }
+
+        public IActionResult UpdateQuiz([FromBody] QuizModel quizModel)
+        {
+            return null;
+        }
+
+        public IActionResult ExcluirQuiz([FromBody] QuizModel quizModel)
+        {
+            return null;
+        }
+    }
+}
---
+++ GETAF/Models/ViewModel/QuizModel.cs
@@ -0,0 +1,90 @@
+﻿using GETAF.Models.Context;
+using GETAF.Models.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.Net.NetworkInformation;
+
+namespace GETAF.Models.ViewModel
+{
+    public class QuizModel
+    {
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+        public int GrupoId { get; set; }
+        [Required]
+        public required string Titulo { get; set; }
+        public string Descricao { get; set; }
+
+        public Quiz? CriarQuiz(AppDbContext _context)
+        {
+            try
+            {
+                var quiz = new Quiz
+                {
+                    Titulo = Titulo,
+                    Descricao = Descricao,
+                    UsuarioId = UsuarioId,
+                    GrupoId = GrupoId
+                };
+                _context.Quiz.Add(quiz);
+                _context.SaveChanges();
+                return quiz;
+            }
+            catch (Exception ex)
+            {
+
+                return null;
+            }
+        }
+
+        public List<Quiz> ListarQuiz(AppDbContext _context)
+        {
+            try
+            {
+                return _context.Quiz.Where(x => x.GrupoId == GrupoId).ToList();
+            }
+            catch (Exception)
+            {
+                return new List<Quiz>();
+            }
+        }
+
+        public Quiz? UpdateQuiz(AppDbContext _context)
+        {
+            try
+            {
+                var quiz = _context.Quiz.Find(Id);
+                if (quiz == null) return null;
+
+                quiz.Titulo = Titulo;
+                quiz.Descricao = Descricao;
+                quiz.UsuarioId = UsuarioId;
+                quiz.GrupoId = GrupoId;
+
+                _context.Quiz.Update(quiz);
+                _context.SaveChanges();
+                return quiz;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public Quiz? ExcluirQuiz(AppDbContext _context)
+        {
+            try
+            {
+                var quiz = _context.Quiz.Find(Id);
+                if (quiz == null) return null;
+
+                _context.Quiz.Remove(quiz);
+                _context.SaveChanges();
+                return quiz;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
